f0322798c3ab4f99c1ab68308d55942d2089ead4
FRDev66 <frdev66@amp66.fr>
2022-11-14 22:59:30 +0100

    ESP8266 : Amélioration du Code de Connexion + Scan des SSID disponible
    Signed-off-by: FRDev66 <frdev66@amp66.fr>

--- station_meteo_v1.0/script_connexion_Wifi_ESP8266.ino/script_connexion_Wifi_ESP8266/script_connexion_Wifi_ESP8266.ino
+++ station_meteo_v1.0/script_connexion_Wifi_ESP8266.ino/script_connexion_Wifi_ESP8266/script_connexion_Wifi_ESP8266.ino
@@ -1,22 +1,56 @@
+/*
+Ce programme permet de chercher tous les accès wifi disponibles à portée
+*/
 #include <ESP8266WiFi.h>
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  // déconnection WiFi si une connexion préalable était faite
+  // WiFi.mode(WIFI_STA);
+  // WiFi.disconnect();
+  // delay(100);
+
+  // Serial.println("Fait");
+
+  // put your setup code here, to run once:
+  //Serial.begin(115200);
   WiFi.begin("Livebox-1F90", "o3jwTuDzadcmQAtZ2r");
 
   Serial.print("Connection en cours");
   while (WiFi.status() != WL_CONNECTED) // tant que l'esp n'est pas connecté au réseau on attends
   {
-    delay(500);
+    delay(3000);
     Serial.print(".");
   }
   Serial.println();
   Serial.print("Connecté, adresse IP : ");
   Serial.println(WiFi.localIP()); // affichage de l'adresse IP
-}
+  
+} 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Serial.println("scan demarre");
+  // // WiFi.scan donne le nombre de points wifi disponibles
+  // int n = WiFi.scanNetworks();
+  // Serial.println("scan terminé");
+  // if (n == 0) {
+  //   Serial.println("aucun point");
+  // } else {
+  //   Serial.print(n);
+  //   Serial.println(" nombre trouvé :");
+  //   for (int i = 0; i < n; ++i) {
+  //     // Print SSID and RSSI
+  //     Serial.print(i + 1);
+  //     Serial.print(": ");
+  //     Serial.print(WiFi.SSID(i));
+  //     Serial.print(" (");
+  //     Serial.print(WiFi.RSSI(i));
+  //     Serial.print(")");
+  //     Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+  //     delay(10);      
+  //   }
+  // }
+  // Serial.println("");
+  // // attente pour un nouveau scan
+  // delay(5000);
 }
