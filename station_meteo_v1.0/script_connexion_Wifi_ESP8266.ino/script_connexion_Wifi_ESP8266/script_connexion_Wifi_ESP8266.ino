d61ae1ea47b083084bb2bf64658f37fbc92f3777
FRDev66 <frdev66@amp66.fr>
2022-11-14 23:36:48 +0100

    ESP8266 : Connexion à BLYNK --> OK
    Signed-off-by: FRDev66 <frdev66@amp66.fr>

--- station_meteo_v1.0/script_connexion_Wifi_ESP8266.ino/script_connexion_Wifi_ESP8266/script_connexion_Wifi_ESP8266.ino
+++ station_meteo_v1.0/script_connexion_Wifi_ESP8266.ino/script_connexion_Wifi_ESP8266/script_connexion_Wifi_ESP8266.ino
@@ -2,6 +2,11 @@
 Ce programme permet de chercher tous les accès wifi disponibles à portée
 */
 #include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define BLYNK_PRINT Serial // Enables Serial Monitor
+
+char auth[] = "fbEW6FfjpWu-8CUdMiJP0DXMuMfbYakg";
 
 void setup() {
   Serial.begin(115200);
@@ -13,7 +18,6 @@ void setup() {
   // Serial.println("Fait");
 
   // put your setup code here, to run once:
-  //Serial.begin(115200);
   WiFi.begin("Livebox-1F90", "o3jwTuDzadcmQAtZ2r");
 
   Serial.print("Connection en cours");
@@ -26,9 +30,16 @@ void setup() {
   Serial.print("Connecté, adresse IP : ");
   Serial.println(WiFi.localIP()); // affichage de l'adresse IP
   
+
+  // Here your Arduino connects to the Blynk Cloud.
+  Blynk.begin(auth,"Livebox-1F90", "o3jwTuDzadcmQAtZ2r");
 } 
 
 void loop() {
+  // All the Blynk Magic happens here...
+  Blynk.run();
+  Blynk.notify("IP : 192.168.1.39");
+  
   // Serial.println("scan demarre");
   // // WiFi.scan donne le nombre de points wifi disponibles
   // int n = WiFi.scanNetworks();
