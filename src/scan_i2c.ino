ab0afaeee918d47dcc81b28dd38bd3d90f4d3a6c
FRDev66 <frdev66@amp66.fr>
2022-12-28 22:30:50 +0100

    Signed-off-by: FRDev66 <frdev66@amp66.fr>

--- src/scan_i2c.ino
+++ src/scan_i2c.ino
@@ -1,22 +1,14 @@
-/*  Description :   Scanne les 127 adresses i2c possibles, à la recherche de périphériques connectés sur l'Arduino
-                  (affiche l'adresse des périphériques détectés)
-
-  Auteur :        Jérôme TOMSKI (https://passionelectronique.fr/)
-  Créé le :       25.07.2021
-
-*/
-
 #include <Wire.h>                         // Inclus la bibliothèque Wire.h, afin de pouvoir envoyer des instructions sur le port i2c
 
-byte nombreDePeripheriquesTrouves = 0;    // Variable indiquant combien de périphériques I2C ont répondu "présent"
+int nombreDePeripheriquesTrouves = 0;    // Variable indiquant combien de périphériques I2C ont répondu "présent"
 
 // ========================
 // Initialisation programme
 // ========================
 void setup() {
 
-  // Initialisation de la liaison série, côté PC (pour y faire afficher des infos, via le moniteur série de IDE Arduino)
-  Serial.begin(9600);
+  Serial.begin(115200);
+
   Serial.println("                                    ~~ SCANNER I2C ~~                                       ");
   Serial.println("Scanne toutes les adresses i2c, afin de repérer tous les périphériques connectés à l'arduino");
   Serial.println("============================================================================================");
@@ -26,16 +18,16 @@ void setup() {
   Wire.begin();
 
   // Boucle de parcous des 127 adresses i2c possibles
-  for (byte adresseI2C = 0; adresseI2C < 127; adresseI2C++)
+  for (int adresseI2C = 0; adresseI2C < 127; adresseI2C++)
   {
     Wire.beginTransmission(adresseI2C);             // Interrogation de l'adresse i2c ciblée
     if (Wire.endTransmission () == 0)               // Si cela s'est bien passé, c'est qu'il y a un périphérique connecté à cette adresse
     {
-      Serial.print(F("Périphérique i2c trouvé à l'adresse : "));
+      Serial.print("Périphérique i2c trouvé à l'adresse : ");
       Serial.print(adresseI2C, DEC);                // On affiche son adresse au format décimal
-      Serial.print(F(" (0x"));
+      Serial.print(" (0x");
       Serial.print(adresseI2C, HEX);                // … ainsi qu'au format hexadécimal (0x..)
-      Serial.println(F(")"));
+      Serial.println(")");
       
       nombreDePeripheriquesTrouves++;
       delay(1);                                     // Temporisation, avant de passer au scan de l'adresse suivante
@@ -44,18 +36,18 @@ void setup() {
 
   // Affichage final, indiquant le nombre total de périphériques trouvés sur le port I2C de l'arduino
   if (nombreDePeripheriquesTrouves == 0) {
-    Serial.println(F("Aucun périphérique I2C trouvé…"));
+    Serial.println("Aucun périphérique I2C trouvé…");
   }
   else if (nombreDePeripheriquesTrouves == 1) {
     Serial.println();
-    Serial.println(F("1 périphérique trouvé !"));
+    Serial.println("1 périphérique trouvé !");
   }
   else {
     Serial.println();
     Serial.print(nombreDePeripheriquesTrouves);
-    Serial.println(F("périphériques trouvés !"));
+    Serial.println("périphériques trouvés !");
   }
-  Serial.println(F("Scan terminé."));  
+  Serial.println("Scan terminé.");  
   
 }
 
