74e2c75cc3990f5369a438409ab022ce7cd3a61c
FRDev66 <frdev66@amp66.fr>
2023-01-13 19:35:55 +0100

    Correction Bug Température négaitve
    Signed-off-by: FRDev66 <frdev66@amp66.fr>

--- src/station_meteo_v1.1.0.ino
+++ src/station_meteo_v1.1.0.ino
@@ -16,75 +16,130 @@
   See the LICENSE file for details.
  ***************************************************************************/
 
+#define BLYNK_TEMPLATE_ID           "TMPLvMXPVYpR"
+#define BLYNK_DEVICE_NAME           "Meteo"
+#define BLYNK_AUTH_TOKEN            "e4Ra7py3GsW9pAvPpFUZwiwB17pZIYHJ"
+
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
+#define BLYNK_PRINT Serial
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+//#define BLYNK_PRINT Serial // Enables Serial Monitor
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "Livebox-1F90";
+char pass[] = "o3jwTuDzadcmQAtZ2r";
+
+// Constantes du programme
+#define adresseI2CduBME280 0x76              // Adresse I2C du BME280 (0x76, dans mon cas, ce qui est souvent la valeur par défaut)
+#define SEALEVELPRESSURE_HPA 1024.90         // https://fr.wikipedia.org/wiki/Pression_atmospherique (1013.25 hPa en moyenne, valeur "par défaut")
+#define delaiRafraichissementAffichage 1500  // Délai de rafraîchissement de l'affichage (en millisecondes)
 
 Adafruit_BME280 bme; // I2C
 //Adafruit_BME280 bme(BME_CS); // hardware SPI
 //Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
 
-unsigned long delayTime;
+byte nombreDePeripheriquesTrouves = 0;    // Variable indiquant combien de périphériques I2C ont répondu "présent"
+
+//unsigned long delayTime;
+unsigned long tempoDepart = 0;
 
 void setup() {
-    Serial.begin(115200);
-    while(!Serial);    // time to get serial running
-    Serial.println(F("BME280 test"));
+  Serial.begin(115200);
+  Serial.println(F("                                    ~~ SCANNER I2C ~~                                       "));
+  Serial.println(F("Scanne toutes les adresses i2c, afin de repérer tous les périphériques connectés à l'arduino"));
+  Serial.println(F("============================================================================================"));
+  Serial.println();
 
-    unsigned status;
-    
-    // default settings
-    status = bme.begin();  
-    // You can also pass in a Wire library object like &Wire2
-    // status = bme.begin(0x76, &Wire2)
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(10);
+  // Initialisation de la liaison i2C
+  Wire.begin();
+
+  // Boucle de parcous des 127 adresses i2c possibles
+  for (byte adresseI2C = 0; adresseI2C < 127; adresseI2C++)
+  {
+    Wire.beginTransmission(adresseI2C);             // Interrogation de l'adresse i2c ciblée
+    if (Wire.endTransmission () == 0)               // Si cela s'est bien passé, c'est qu'il y a un périphérique connecté à cette adresse
+    {
+      Serial.print(F("Périphérique i2c trouvé à l'adresse : "));
+      Serial.print(adresseI2C, DEC);                // On affiche son adresse au format décimal
+      Serial.print(F(" (0x"));
+      Serial.print(adresseI2C, HEX);                // … ainsi qu'au format hexadécimal (0x..)
+      Serial.println(F(")"));
+      
+      nombreDePeripheriquesTrouves++;
+      //delay(1);                                     // Temporisation, avant de passer au scan de l'adresse suivante
     }
-    
-    Serial.println("-- Default Test --");
-    delayTime = 1000;
+  }
 
+  // Affichage final, indiquant le nombre total de périphériques trouvés sur le port I2C de l'arduino
+  if (nombreDePeripheriquesTrouves == 0) {
+    Serial.println(F("Aucun périphérique I2C trouvé…"));
+  }
+  else if (nombreDePeripheriquesTrouves == 1) {
     Serial.println();
+    Serial.println(F("1 périphérique trouvé !"));
+  }
+  else {
+    Serial.println();
+    Serial.print(nombreDePeripheriquesTrouves);
+    Serial.println(F("périphériques trouvés !"));
+  }
+  Serial.println(F("Scan terminé."));  
+
+  if(!bme.begin(adresseI2CduBME280)) {
+    Serial.println(F("--> ÉCHEC…"));
+  } else {
+    Serial.println(F("--> RÉUSSIE !"));
+  }
+  Serial.println();
+
+  Blynk.begin(auth, ssid, pass);
 }
 
 
 void loop() { 
-    printValues();
-    delay(delayTime);
+    //printValues();
+    //delay(delayTime);
+
+  if ( millis() - tempoDepart >= 5000 ) {
+  tempoDepart = millis();
+  mesure_temp_humidite();
+  // Et on désactive la temporisation pour ne pas afficher ce message une seconde fois
+  //tempoActive = 0; 
+  }
 }
 
 
-void printValues() {
-    Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" °C");
+void mesure_temp_humidite() {
+  float temperature = bme.readTemperature();
+  float pression = bme.readPressure() / 100.0F;
+  float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  float humidity = bme.readHumidity();
+  
+  Serial.print("Temperature = ");
+  Serial.print(temperature);
+  Serial.println(" °C");
 
-    Serial.print("Pressure = ");
+  Serial.print("Pression Atmospherique = ");
+  Serial.print(pression);
+  Serial.println(" hPa");
 
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
+  Serial.print("Approx. Altitude = ");
+  Serial.print(altitude);
+  Serial.println(" m");
 
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.println(" m");
+  Serial.print("Humidite = ");
+  Serial.print(humidity);
+  Serial.println(" %");
 
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
+  Serial.println();
 
-    Serial.println();
+  Blynk.virtualWrite(V6,temperature);
+  Blynk.virtualWrite(V4,humidity);
+  Blynk.virtualWrite(V5,pression);
+  Blynk.virtualWrite(V7,altitude);
 }
